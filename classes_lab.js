495034d0cc365b0c2d53972752db5f1a971cea55
tawnyblvd <margara.sees@gmail.com>
2019-07-30 20:11:39 -0400

    update for q1.a response

--- classes_lab.js
+++ classes_lab.js
@@ -4,9 +4,21 @@
 //
 // ## Question 1
 //
-// a. Write a class called `Person` that has 3 properties: a first name, a last name and a middle name. Create 2 instances of a `Person`. Print one of their first names.
-//
-//
+// a. Write a class called `Person` that has 3 properties: a first name, a last name and a middle name.
+// Create 2 instances of a `Person`. Print one of their first names.
+class Person {
+  constructor (firstName, lastName, middleName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.middleName = middleName;
+  }
+}
+
+let kai = new Person("kai", "lovelace", "elvis");
+let dahls = new Person("dahls", "rivera", "elisa");
+
+console.log(dahls.firstName);
+
 // b. Write a method in `Person` called `fullName` that will return a formatted string of an instance's full name. Call this method on both the instances you created in part a.
 //
 //
